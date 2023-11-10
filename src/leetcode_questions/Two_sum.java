7cc895282f64f5d3d87bbc398b3241622c99b6d7
Piyush-Agrawal7 <132205178+Piyush-Agrawal7@users.noreply.github.com>
2023-11-10 20:06:44 +0530

    first commit

---
+++ src/leetcode_questions/Two_sum.java
@@ -0,0 +1,11 @@
+package leetcode_questions;
+
+public class Two_sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi my name is ayush agrawal .. ");
+
+	}
+
+}
